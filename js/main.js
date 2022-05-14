627f537f6c4fee51e6c429d5ad4eb0f86c85996d
Pablo Moreno <pablolm97@gmail.com>
2022-05-14 10:04:03 -0500

    Rooms for pets created

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-class Pets {
+class Pet {
     constructor(petName, petSize, petWeight, petAge, petDangerousToHumans, petDangerousToPets, petSick, petInSpecialTreatment){
     this.petName = petName;
     this.petSize = petSize;
@@ -14,4 +14,19 @@ class Pets {
     }
 }
 
-const Pets = [];
+class Room {
+    constructor(petsCapacity, petsInside){
+        this.petsCapacity = petsCapacity;
+        this.petsInside = petsInside;
+    }
+}
+const room1 = new Room(3, 0)
+const room2 = new Room(3, 0)
+const room3 = new Room(2, 0)
+const room4 = new Room(5, 0)
+const room5 = new Room(3, 0)
+const room6 = new Room(2, 0)
+const room7 = new Room(2, 0)
+const room8 = new Room(1, 0)
+const room9 = new Room(1, 0)
+const room10 = new Room(1, 0)
