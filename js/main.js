278f3de528b4aeb0d7812805c4a15f78c34d816a
Pablo Moreno <pablolm97@gmail.com>
2022-05-14 09:26:37 -0500

    Adding animate.css library to headers

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 class NewPets {
     constructor(petName, petType, petSize, petAge){
-        this.name = petName;
-        this.type = petType;
-        this.size = petSize;
-        this.age = petAge;
+        this.petName = petName;
+        this.petType = petType;
+        this.petSize = petSize;
+        this.petAge = petAge;
     }
     petAdded(){
-        return `Se ha agregado a ${this.name} de ${this.age} años y estatura ${this.size} a la lista de ${this.type}s`
+        return `Se ha agregado a ${this.petName} de ${this.petAge} años y estatura ${this.petSize} a la lista de ${this.petType}s`
     }
 }
 
@@ -15,50 +15,3 @@ let petType = 0;
 let petSize = 0;
 let ageIsNotNumeric = true;
 
-petName = prompt('Ingresa el nombre de la nueva mascota');
-
-while (petType == 0){
-    petType = prompt('Ingresa el número correspondiente al tipo de la nueva mascota: 1)Gato - 2)Perro');
-    if (petType == 1){
-        petType = "gato";
-    } else if (petType ==2){
-        petType ="perro";
-    } else{
-        alert(`Por favor ingrese un tipo válido`);
-        petType = 0;
-    }
-}
-
-while(petSize == 0){
-    petSize = Number(prompt(`Ingresa el número correspondiente al tamaño del nuevo ${petType}: 1)Pequeño - 2)Mediano - 3)Grande - 4)Gigante`));
-    switch(petSize){
-        case 1:
-            petSize = "pequeño";
-            break;
-        case 2:
-            petSize = "mediano";
-            break;
-        case 3:
-            petSize = "grande";
-            break;
-        case 4:
-            petSize = "gigante";
-            break;
-        default:
-            petSize = 0;
-    }
-    if (petSize == 0){
-        alert("Por favor ingrese un número válido");
-    }
-}
-
-while (ageIsNotNumeric == true) {
-    petAge = prompt('Ingresa la edad de la mascota en años');
-    ageIsNotNumeric = isNaN(petAge)
-    if(ageIsNotNumeric == true){
-        alert("Por favor ingrese un valor numérico de edad")
-    }
-}
-
-newPets.push(new NewPets (petName, petType, petSize, petAge));
-alert(newPets[0].petAdded());
