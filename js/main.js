b61f1935bca1acca0780954022cdbeda2e34f52c
Pablo Moreno <pablolm97@gmail.com>
2022-05-28 13:24:48 -0500

    including toastify library to notify

--- js/main.js
+++ js/main.js
@@ -109,7 +109,10 @@ function addPet(event){
     updateHTML(dogsInShelter);
     Toastify({
         text: `¡Se ha agregado a ${incomeForm[0].value} a las mascotas!`,
-        duration: 3000
+        duration: 3000,
+        style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
     }).showToast();
     incomeForm.reset();
 }
